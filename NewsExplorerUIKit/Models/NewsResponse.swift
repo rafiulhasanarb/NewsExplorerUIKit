1030b2eaf844326e18b1ad4ce9c484ed7dc989b2
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-06-13 15:28:35 +0600

    data model added

---
+++ NewsExplorerUIKit/Models/NewsResponse.swift
@@ -0,0 +1,23 @@
+//
+//  NewsResponse.swift
+//  NewsExplorerUIKit
+//
+//  Created by Rafiul Hasan on 6/13/25.
+//
+
+import Foundation
+
+// MARK: - NewsResponse
+struct NewsResponse: Codable {
+    var status: String
+    var totalResults: Int
+    var articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    var title, description: String
+    var urlToImage: String?
+    var publishedAt: String
+    var content: String
+}
